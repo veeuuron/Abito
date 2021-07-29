2ad08e906b9c6716400beb9d611fd1936bb5c6fa
Viktor <88035318+veeuuron@users.noreply.github.com>
2021-07-29 22:16:20 +0300

    Адаптирована главная страница

--- js/main.js
+++ js/main.js
@@ -11,3 +11,12 @@ var swiper2 = new Swiper(".mySwiper2", {
     swiper: swiper,
   },
 });
+const  menuBtn = document.querySelector(".menu-button");
+const menu = document.querySelector(".navbar-panel");
+const menuCloseBtn = document.querySelector(".close-menu");
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("is-open");
+});
+menuCloseBtn.addEventListener("click", () => {
+  menu.classList.toggle("is-open");
+});
